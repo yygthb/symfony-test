8d78c17fe20d65c8f778d7703946a3f01dbbbdf2
Yura Ya <yy.ainu.dev@gmail.com>
2022-05-17 12:47:52 +0300

    feat: vue template at vue-route

---
+++ assets/js/log.js
@@ -0,0 +1,1 @@
+console.log('symfony app');
---
+++ assets/js/vue/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app">
+    <h2>text from Vue</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.app {
+  margin: 20px 0;
+  color: rgb(0, 110, 255);
+}
+</style>
